c3b13294384ce16f7e15deaf3c6721104f1396c5
guytchoupo <guytchoupo@gmail.com>
2022-02-21 10:00:56 -0500

    update2

--- next-blog-ui/src/modules/authentication/components/login/sign-in-form.tsx
+++ next-blog-ui/src/modules/authentication/components/login/sign-in-form.tsx
@@ -1,19 +1,13 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
+
 import FormFieldText from "@next/components/form/field/text";
 import Form from "@next/components/form";
-import * as yup from "yup";
+
 import { SignInModel } from "../../types";
 import Button from "@next/components/button";
 import { signInFormSchema } from "../../utils";
-
-const schema = yup.object({
-  username: yup.string().email().required(),
-  password: yup.string().required(),
-});
 
 const SignInForm: React.FC = () => {
   const handleSingIn = (values: SignInModel) => {
--- next-blog-ui/src/pages/index.tsx
+++ next-blog-ui/src/pages/index.tsx
@@ -1,15 +1,8 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import SignInForm from "@next/modules/authentication/components/login";
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <SignInForm />
-    </div>
-  );
+  return <div className={styles.container}>Login form</div>;
 };
 
 export default Home;
