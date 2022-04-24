a0ecf8aebb96e5565231ed0daede4f4d53a9171b
guytchoupo <guytchoupo@gmail.com>
2022-04-23 22:25:46 -0400

    update header logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@next/styles/styles.scss";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
+import Head from "next/head";
 import store from "@next/store/configure-store";
 import WithAuth from "@next/modules/authentication/components/with-auth";
 import { Layout, Menu, Breadcrumb } from "antd";
@@ -11,6 +12,9 @@ const { Header, Content, Footer } = Layout;
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Head>
+        <link rel="shortcut icon" href="/head-icon.png" />
+      </Head>
       {/* <WithAuth> */}
       <AppLayout>
         <Component {...pageProps} />
