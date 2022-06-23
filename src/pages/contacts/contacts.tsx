a0570ec436f2592bd51216d1c708f9809d4ed1c4
guytchoupo <guytchoupo@gmail.com>
2022-06-23 10:37:34 -0400

    remove deaad code

--- src/pages/contacts/contacts.tsx
+++ src/pages/contacts/contacts.tsx
@@ -1,18 +1,28 @@
-import PageTitle from "@next/components/page-title";
 import React from "react";
+import PageTitle from "@next/components/page-title";
+import { Space, Typography } from "antd";
 
+const { Text, Link } = Typography;
 const Contacts: React.FC = () => {
   return (
     <section>
       <PageTitle title="Contacts" />
-      <p>guytchoupo@gmail.com</p>
-      <p>+1 438 580 5709</p>
-      <p>Montreal,QC</p>
       <p>
-        {" "}
-        <a href="https://www.linkedin.com/in/guy-tchoupo">
-          https://www.linkedin.com/in/guy-tchoupo
-        </a>
+        <Text>guytchoupo@gmail.com</Text>
+      </p>
+      <p>
+        <Text>+1 438 580 5709</Text>
+      </p>
+      <p>
+        <Text>Montreal,QC</Text>
+      </p>
+      <p>
+        <Link
+          target="_blank"
+          href="https://www.linkedin.com/in/guy-tchoupo-945560156/"
+        >
+          linkedin profile
+        </Link>
       </p>
     </section>
   );
