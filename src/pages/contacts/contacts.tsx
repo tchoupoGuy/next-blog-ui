d15ee6a35629d25458d1dad01b9d502d9f57cf28
guytchoupo <guytchoupo@gmail.com>
2022-06-23 09:47:26 -0400

    add contact page

--- src/pages/contacts/contacts.tsx
+++ src/pages/contacts/contacts.tsx
@@ -5,6 +5,10 @@ const Contacts: React.FC = () => {
   return (
     <section>
       <PageTitle title="Contacts" />
+      <p>guytchoupo@gmail.com</p>
+      <p>+1 438 580 5709</p>
+      <p>Montreal,QC</p>
+      <p>https://www.linkedin.com/in/guy-tchoupo</p>
     </section>
   );
 };
