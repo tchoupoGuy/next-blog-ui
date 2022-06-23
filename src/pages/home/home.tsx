39796a9f0ea69f9ce6faa84965d29ff9e19fcd39
guytchoupo <guytchoupo@gmail.com>
2022-06-23 15:26:02 -0400

    add linkedin icon

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,10 +2,16 @@ import styles from "./home.module.scss";
 import React from "react";
 import Image from "next/image";
 
-import { Typography } from "antd";
+import { Avatar, Typography } from "antd";
 import profilePicture from "@next/public/assets/img/profile-picture-two.jpg";
+import { GithubFilled, LinkedinFilled } from "@ant-design/icons";
 
 const HomePage: React.FC = () => {
+  const handleOpenLinkedinProfile = () => {
+    window.open("https://www.linkedin.com/in/guy-tchoupo-945560156/", "_blank");
+  };
+  const HIDE_ICON = false;
+
   return (
     <>
       <section className={styles.cHomePage}>
@@ -19,6 +25,26 @@ const HomePage: React.FC = () => {
           <Typography.Title level={4} className={styles.jobPositionContainer}>
             Frontend developer
           </Typography.Title>
+          <div className={styles.cHomePage__iconContainer}>
+            <Avatar
+              icon={
+                <LinkedinFilled
+                  className={styles.iconContainer__color}
+                  onClick={handleOpenLinkedinProfile}
+                />
+              }
+              size={70}
+              className={styles.iconContainer}
+            />
+
+            {HIDE_ICON && (
+              <Avatar
+                size={70}
+                icon={<GithubFilled className={styles.iconContainer__color} />}
+                className={styles.iconContainer}
+              />
+            )}
+          </div>
         </div>
         <div className={styles.cHomePage__imgContainer}>
           <Image
