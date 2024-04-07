f176950b272686a88135f09ca733b97cad767373
guytchoupo <guytchoupo@gmail.com>
2024-04-07 17:56:18 -0400

    fix conflicts

--- src/modules/authentication/components/with-auth.tsx
+++ src/modules/authentication/components/with-auth.tsx
@@ -4,10 +4,14 @@ import SignInForm from "./login";
 
 // import { selectIsUserAuthenticated } from '@kaayu/modules/authentication/selectors';
 // import Authentication from '@kaayu/modules/authentication/container';
+<<<<<<< HEAD
 // add new comment in develop
 // add second comment in develop
 // add second comment in the LEAR-GIT-STASH branch
 
+=======
+// add new comment in remove dead code branch
+>>>>>>> bf4d7e3 (add new comment is remove dead code branch)
 interface WithAuthProps {
   children: ReactNode;
 }
