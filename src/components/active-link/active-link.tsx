98b78590816198b903e2915e8b730dc8e380cfea
guytchoupo <guytchoupo@gmail.com>
2024-04-07 17:20:37 -0400

    update node version

--- src/components/active-link/active-link.tsx
+++ src/components/active-link/active-link.tsx
@@ -12,21 +12,18 @@ const ActiveLink: React.FC<PropsWithChildren<ActiveLinkProps>> = ({
   id,
 }) => {
   const router = useRouter();
-
+  const style = {
+    marginLeft: 16,
+    color: router.asPath === href ? '#1e76e3' : '#000',
+    fontWeight: 'bold',
+  };
   const handleClick = (e: any) => {
     e.preventDefault();
     router.push(href);
   };
 
   return (
-    <a
-      key={id}
-      href={href}
-      onClick={handleClick}
-      className={` ${styles.activeLink} ${
-        router.asPath ? styles.linkHoverColor : styles.linkBlackColor
-      }`}
-    >
+    <a key={id} href={href} onClick={handleClick} style={style}>
       {children}
     </a>
   );
